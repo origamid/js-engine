9d0901b368f0a5a861d92e0722e0c0eac24fd2ed
Andre Rafael <andre@origamid.com>
2018-08-14 04:11:25 +0100

    First commit

---
+++ readme.md
@@ -0,0 +1,8 @@
+## JS Engine
+
+Simulação de como o código JavaScript é executado.
+
+A mesma não representa a realidade, mas sim as principais etapas do processo de execução do código.
+
+Inspirado pelo projeto Loupe de Philip Roberts <http://latentflip.com/loupe/>
+E pelo post de Jake Archibald explicando tasks e microtasks <https://jakearchibald.com/2015/tasks-microtasks-queues-and-schedules/>
---
+++ script.js
@@ -0,0 +1,73 @@
+Prism.highlightAll();
+
+// worst code ever
+let dom = document.createElement('div');
+const etapaCounter = document.querySelector('.etapa');
+
+function fetchEtapa(etapa = 0) {
+  etapaCounter.innerText = etapa;
+  fetch(`exemplos/${etapa}.html`).then(r => r.text())
+  .then(r => {
+    dom.innerHTML = r;
+    const elements = Array.from(dom.children);
+    elements.forEach(element => {
+      const actualElement = document.getElementById(element.id);
+      if(element.id === 'code') {
+        actualElement.nextElementSibling.dataset.line = element.dataset.line;
+        Prism.highlightAll();
+        console.log(element.dataset.line);
+        if(element.dataset.line === '0')
+          setTimeout(()=> document.querySelector('.line-highlight').remove());
+        if(element.dataset.queue)
+          document.querySelector('#event').classList.add('animate');
+        else
+          document.querySelector('#event').classList.remove('animate');
+      }
+      actualElement.innerHTML = element.innerHTML;
+    })
+  }).then(() => {
+    const remover = document.querySelectorAll('[data-remover]');
+    setTimeout(() => {
+      remover.forEach(item => item.remove());
+    }, 1000)
+  })
+}
+
+fetchEtapa(0)
+
+function initNav() {
+  const proximo = document.querySelector('.proximo');
+  const anterior = document.querySelector('.anterior');
+  proximo.addEventListener('click', handleProximo);
+  anterior.addEventListener('click', handleAnterior);
+  document.addEventListener('keydown', handleKey);
+
+  function handleKey(event) {
+    if(event.code === 'ArrowLeft') {
+      handleAnterior()
+    } else if(event.code === 'ArrowRight') {
+      handleProximo()
+    }
+  }
+
+  let i = 0;
+  let total = 13;
+  function handleProximo() {
+    if(i < total)
+      fetchEtapa(++i);
+    else if (i === total) {
+      i = 0;
+      fetchEtapa(i);
+    }
+  }
+
+  function handleAnterior() {
+    if(i >= 1)
+      fetchEtapa(--i);
+    else if (i === 0) {
+      i = total;
+      fetchEtapa(i);
+    }
+  }
+}
+initNav();
